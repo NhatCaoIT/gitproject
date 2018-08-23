51d4e1e14f9023952a1fef53925c4393cb07bba2
= <=>
2018-08-23 09:33:15 +0700

    add data

---
+++ Database.java
@@ -0,0 +1,16 @@
+package C.Models;
+
+public class Database {
+	public boolean checkUser(String Username , String Password){
+		if (Username.equals("HeuKeu")&& Password.equals("1997")){
+			return true;
+		
+		}
+		else {
+			return false;
+		}
+		}
+		
+	}
+
+
