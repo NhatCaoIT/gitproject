b74b7132dbe5032f449619b28dd8b8957bf898f5
= <=>
2018-08-23 09:32:14 +0700

    add file

---
+++ Data.java
@@ -0,0 +1,17 @@
+package C.Models;
+
+public class Data {
+	public boolean Check1(String username, String Pass){
+		
+		if(username=="nhatcao"&& Pass == "1997"){
+			return true;
+			
+		}
+		else{
+				return false;
+				
+		}
+	}
+	
+
+}
